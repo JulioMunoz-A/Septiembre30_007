f1a954fbd5e9453f117be47e6b7378b6cd2bd9a2
julio muñoz <julioesteban1661@gmail.com>
2020-09-30 13:16:15 -0300

    Se agrego un menú de opciones

---
+++ Menu.py
@@ -0,0 +1,45 @@
+import os
+def Numeros():
+    print("**** Opción de Números ****")
+    #ingresar n números, donde n es un número ingresado por teclado, calcular y mostrar: 
+    #cantidad de números positivos, cantidad de números negativos, y cantidad de iguales a cero
+
+    veces= int(input("Cuantos números desea ingresar?: "))
+    pos=0
+    neg=0
+    cero=0
+    for x in range(veces): 
+        nume=int(input("Ingrese un número: "))
+        if (nume>0):
+            pos=pos+1
+        elif(nume<0):
+            neg=neg+1
+        else:
+            cero=cero+1
+            
+     print("Cantidad de números positivos: " + str(pos) +
+      "\n Cantidad de números negativos : "+ str(neg)+ 
+          "\n Cantidad de números iguales a cero: " + str(cero)) 
+
+    Pausa=input("Presiones una tecla para continuar")
+
+def Datos():
+    print("**** Opción de Datos de Personas ****")
+    Pausa=input("Presiones una tecla para continuar")
+
+seguir=True
+while (seguir):
+    os.system('cls')
+    print("1. Opción Numeros ")
+    print("2. Opción Datos de Personas  ")
+    print("3. Finalizar ")
+    op=int(input("Ingrese opción 1, 2, 3: "))
+    if (op==1):
+        Numeros()
+    if (op==2):
+        Datos()
+    if (op==3):
+        seguir=False
+        break
+
+   
