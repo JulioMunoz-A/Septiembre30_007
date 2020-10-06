6b6729b7ee281f11e1c7f35e2c14d974cb791dd9
julio muñoz <julioesteban1661@gmail.com>
2020-10-06 12:53:19 -0300

    se programo la opción 2 del menú

--- Menu.py
+++ Menu.py
@@ -18,9 +18,9 @@ def Numeros():
         else:
             cero=cero+1
 
-      print("Cantidad de números positivos: " + str(pos)+ 
-          "\n Cantidad de números negativos : "+ str(neg)+ 
-          "\n Cantidad de números iguales a cero: " + str(cero))    
+        print("Cantidad de números positivos: " + str(pos) +
+            "\n Cantidad de números negativos : " + str(neg) + 
+            "\n Cantidad de números iguales a cero: " + str(cero))    
 
     Pausa=input("Presiones una tecla para continuar")
 
@@ -29,9 +29,27 @@ def Datos():
 
     #ingresar para n personas donde n es un número ingresado por teclado: nombre y edad. 
     #calcular y mostrar: cantidad de personas mayores de edad y cantidad de menores de edad. 
-    #subir la modificar a github con el siguiente mensaje: "se programo la opción 2 del menú"
+    #subir la modificacion de github con el siguiente mensaje: "se programo la opción 2 del menú"
+
     
-    Pausa=input("Presiones una tecla para continuar")
+    pos=0
+    neg=0
+  
+    veces= int(input("Cuantas personas desea ingresar?: "))
+    for x in range(veces): 
+        nombre=str(input("Ingrese nombre: "))
+        edad=int(input("Ingrese la edad: "))
+        if (edad>18):
+            pos=pos+1
+        elif(edad<18):
+            neg=neg+1
+
+
+        print("Cantidad de personas mayores de edad: " + str(pos)+ 
+            "\n Cantidad de personas mayores de edad : "+ str(neg))
+       
+
+        Pausa=input("Presiones una tecla para continuar")
 
 seguir=True
 while (seguir):
